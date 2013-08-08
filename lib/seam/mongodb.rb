45915a6f7d6dfc0d183d836443ee1a40713c4a46
Darren Cauthon <darren@cauthon.com>
2013-08-08 15:30:13 -0500

    Only overwrite the persistence layer when a connection is set.

--- lib/seam/mongodb.rb
+++ lib/seam/mongodb.rb
@@ -11,40 +11,42 @@ module Seam
 
     def self.set_collection collection
       @collection = collection
+      overwrite_the_persistence_layer
+    end
+
+    def self.overwrite_the_persistence_layer
+      Seam::Persistence.class_eval do
+        def self.find_by_effort_id effort_id
+          document = Seam::Mongodb.collection.find( { id: effort_id } ).first
+          return nil unless document
+          Seam::Effort.parse document
+        end
+
+        def self.find_all_pending_executions_by_step step
+          Seam::Mongodb.collection
+            .find( { next_step: step, next_execute_at: { '$lte' => Time.now } } )
+            .map { |x| Seam::Effort.parse x }
+        end
+
+        def self.save effort
+          Seam::Mongodb.collection
+              .find( { id: effort.id } )
+              .update("$set" => effort.to_hash)
+        end
+
+        def self.create effort
+          Seam::Mongodb.collection.insert(effort.to_hash)
+        end
+
+        def self.all
+          Seam::Mongodb.collection.find.to_a
+        end
+
+        def self.destroy
+          Seam::Mongodb.collection.drop
+        end
+      end
     end
   end
 end
 
-module Seam
-  module Persistence
-    def self.find_by_effort_id effort_id
-      document = Seam::Mongodb.collection.find( { id: effort_id } ).first
-      return nil unless document
-      Seam::Effort.parse document
-    end
-
-    def self.find_all_pending_executions_by_step step
-      Seam::Mongodb.collection
-        .find( { next_step: step, next_execute_at: { '$lte' => Time.now } } )
-        .map { |x| Seam::Effort.parse x }
-    end
-
-    def self.save effort
-      Seam::Mongodb.collection
-          .find( { id: effort.id } )
-          .update("$set" => effort.to_hash)
-    end
-
-    def self.create effort
-      Seam::Mongodb.collection.insert(effort.to_hash)
-    end
-
-    def self.all
-      Seam::Mongodb.collection.find.to_a
-    end
-
-    def self.destroy
-      Seam::Mongodb.collection.drop
-    end
-  end
-end
